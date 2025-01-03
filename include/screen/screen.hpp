3ad049051d182bcb3cd87e48aaa9c34feab49915
VCOliver <victorcruz.01@gmail.com>
2025-01-03 20:45:37 -0300

    Game Over art showing

--- include/screen/screen.hpp
+++ include/screen/screen.hpp
@@ -1,8 +1,20 @@
 #pragma once
 
+#include <string>
+#include <vector>
 #include "actions/position.hpp"
 
 namespace Screen {
+
+    const std::vector<std::string> gameOverArt = {
+    "  SSSSSS   SSSSS  SSS    SSS SSSSSSS     SSSSSS   SS    SS SSSSSSS SSSSSS  ",
+    " SS       SS   SS SSSS  SSSS SS        SS      SS SS    SS SS      SS   SS ",
+    " SS   SSS SSSSSSS SS SSSS SS SSSSS     SS      SS SS    SS SSSSS   SSSSSS  ",
+    " SS    SS SS   SS SS  SS  SS SS        SS      SS  SS  SS  SS      SS   SS ",
+    "  SSSSSS  SS   SS SS      SS SSSSSSS     SSSSSS      SS    SSSSSSS SS   SS "
+    };
+
+
     void init();
     void clear();
     void close();
--- src/screen/screen.cpp
+++ src/screen/screen.cpp
@@ -32,8 +32,22 @@ Position Screen::getScreenSize() {
 
 void Screen::gameOver() {
     clear();
-    mvprintw(0, 0, "Game Over!");
-    printw("Press any key to exit...");
+    int rows, cols;
+    getmaxyx(stdscr, rows, cols);
+    // Calculate start position for centered text
+    int artHeight = gameOverArt.size();
+    int artWidth = gameOverArt[0].size();
+    int startY = (rows - artHeight) / 2;
+    int startX = (cols - artWidth) / 2;
+
+    // Print ASCII art
+    for (size_t i = 0; i < gameOverArt.size(); ++i) {
+        mvprintw(startY + i, startX, "%s", gameOverArt[i].c_str());
+    }
+
+    startY += artHeight + 2; // Move the cursor down
+    mvprintw(startY, cols/2 -10, "Press any key to exit...");
+
     refresh();
     nodelay(stdscr, FALSE);
     getch();
